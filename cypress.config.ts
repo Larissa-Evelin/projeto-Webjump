1474bd04e04adbd4ac58d52757fef3387e2e1e2b
Larissa <larissa_evelin01@outlook.com>
2025-08-20 10:19:41 -0300

    Teste

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from "cypress";
-
 const failFast = require("cypress-fail-fast/plugin"); 
 
 export default defineConfig({
   e2e: {
     baseUrl: "https://magento2-demo.magebit.com",
     specPattern: "cypress/e2e/**/*.cy.ts",
-    //testIsolation: false,
     retries: 0,
     defaultCommandTimeout: 10000, // timeout padrão de comandos
+    screenshotOnRunFailure: true, // captura screenshot ao falhar
+    video: false,                   
+    videosFolder: "cypress/videos",
+    screenshotsFolder: "cypress/screenshots",
     setupNodeEvents(on, config) {
       failFast(on, config);
       return config;
