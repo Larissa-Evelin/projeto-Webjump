06f3e4ddbf0a243c08a5c968030af8c8567ae4b5
Larissa <larissa_evelin01@outlook.com>
2026-01-03 17:19:26 -0300

    F

--- cypress/e2e/utils/email.cy.ts
+++ cypress/e2e/utils/email.cy.ts
@@ -1,4 +1,4 @@
 export function gerarEmailUnico(): string {
-  const timestamp = Date.now(); // pega o horário atual em milissegundos
-  return `teste+${timestamp}@exemplo.com`; // cria email único
+  const timestamp = Date.now(); 
+  return `teste+${timestamp}@exemplo.com`; 
 }
